5bd8ea3ddf5f6d1bc427330c15f0183617e44e50
Ελένη Νουβάκη <43991268+p17nouv@users.noreply.github.com>
2018-11-05 00:28:31 +0200

    Update script_1.js

--- assets/scripts/script_1.js
+++ assets/scripts/script_1.js
@@ -58,8 +58,8 @@ function donutChart() {
             // ===========================================================================================
             // append the pieChartSVG object to the selection
             var pieChartSVG = selection
-                .attr('width', width + margin.left + margin.right)
-                .attr('height', height + margin.top + margin.bottom)
+               // .attr('width', width + margin.left + margin.right)
+               // .attr('height', height + margin.top + margin.bottom)
               .append('g')
                 .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
             // ===========================================================================================
