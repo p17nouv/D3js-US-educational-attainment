cde1c4d9f60f3d6b573e44b452142781b21c6676
Ελένη Νουβάκη <43991268+p17nouv@users.noreply.github.com>
2018-11-05 22:51:20 +0200

    Update script_3.js

--- assets/scripts/script_3.js
+++ assets/scripts/script_3.js
@@ -2,7 +2,7 @@
 // Data Visualization III - Stacked Bar Chart
 
 var 	w = .97*window.innerWidth,
-	margin = {top: .01*w, right: .12*w, bottom: .1*w, left: .04*w},
+	margin = {top: .01*w, right: .14*w, bottom: .1*w, left: .02*w},
 	width = w - margin.left - margin.right,
 	height = Math.min(.7*w, window.innerHeight*.9) - margin.top - margin.bottom;
 
